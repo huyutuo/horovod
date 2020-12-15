a9fc482a69e2564ca4ea64136769fb7595b889d8
huyutuo <923366850@qq.com>
2020-12-15 15:57:41 +0800

    fix

--- horovod/common/ops/cuda_operations.cc
+++ horovod/common/ops/cuda_operations.cc
@@ -18,6 +18,7 @@
 #include "cuda/cuda_kernels.h"
 #include "../message.h"
 #include "../logging.h"
+#include <sys/time.h>
 
 #include <thread>
 
@@ -128,7 +129,7 @@ public:
         double num_of_mb = 1.5 * size * 4 * 8 / (1024 * 1024);
         double avg = 1000 * 1000 * num_of_mb / time_taken;
         ss << "iietest: Processing " << entries.size()
-           << " tensors, total size:" << size;
+           << " tensors, total size:" << size
            << ", 执行" << name << "耗时" << time_taken * 1.0 / 1000 << "ms"
            << ", avg: " <<  avg << "Mbps.";
         LOG(TRACE) << ss.str() << std::endl;
